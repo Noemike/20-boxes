bb337d00a4ee9db524dce75bb4582464d3d95195
Noemike <66320530+Noemike@users.noreply.github.com>
2020-06-05 16:43:27 +0200

    4th exercise

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
-let name='Noemi'
-console.log(name)
+let name1='Noemi'
+console.log(name1)
+
+//Basic building blocks in JavaScript
+
+let myVariable = true;
+if (myVariable === true) {
+    console.log('The condition has been evaluated to true');
+}
+
+let a = 7;
+if (a < 5) {
+    console.log(a/2);
+} else {
+    console.log(a*2);
+}
+
+a = 2;
+if (a < 5) {
+    console.log(a/2);
+} else {
+    console.log(a*2);
+}
+
+
+/*for (let i = 0; i < 21; i++) {
+    console.log(i);
+} */
+
+//Print the even numbers between 0 and 20.
+
+
+//Exercise 1
+//Use your previous for loops and create a function which print the first N numbers.
+
+let bookCount = 21;
+for (let i = 0; i < bookCount; i++) {
+    console.log(i)
+}
+
+let printNumbersTill= (i) => {
+    i>=20;
+}
+
+
+
