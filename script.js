563829a14dbaece130d89ec6896de75907efe49f
Noemike <66320530+Noemike@users.noreply.github.com>
2020-06-08 16:35:13 +0200

    last task not done

--- script.js
+++ script.js
@@ -22,13 +22,41 @@ if (a < 5) {
     console.log(a*2);
 }
 
+console.log('next task')
+
+let bookCount = 10;
+for (let i = 0; i < bookCount; i++) {
+    console.log(i)} 
+
+console.log('next task')
+
+
+for (let i = 0; i < 11; i++) {
+    console.log(i)} 
+
+console.log ('next task')
+
 //Print the even numbers between 0 and 20.
 
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i)} 
 
-let bookCount = 21;
-for (let i = 0; i < bookCount; i++) {
-    console.log(i)
-} 
+console.log ('next task')
+//printNumbersTill(20);  should print 1, 1, 2, ...., 20
+
+let printNumbersTill = numbers => {
+    for (let i = 0; i <= 20; i++) {
+        console.log(numbers) } 
+}
+
+printNumbersTill(20)
+
+
+console.log ('next task')
+//printNumbersTill(15); // should print 1, 1, 2, ...., 15
+
+printNumbersTill(15)
+
 
 let getGreetingTo = name =>  {
     console.log(`Hello, ${name}`);
@@ -36,18 +64,23 @@ let getGreetingTo = name =>  {
 
 console.log(getGreetingTo('Bea'))
 
-//Exercise 1
-//Use your previous for loops and create a function which print the first N numbers.
-/*let printNumbersTill= numbers => {
-    for (let i = 0; i < bookCount; i++) {
-        console.log(i)}
-}
+console.log ('next task')
 
-console.log(printNumbersTill(10))*/
 
-let numbers = ['one', 'three', 'nine', 'eight', 'seven', 'six',]
-let printValues = (numbers) => {
-    for (let i = 0; i < numbers.length; i++) {
-        console.log(numbers[i]); 
+
+let printValues = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      console.log(array[i]);
     }
-}
+  };
+
+printValues([0, 3, 6, 7, 9]);
+
+console.log ('next task')
+
+  let myNumbers = [10, 20, 30, 50, 12];
+  // here we say, we want to do something with each item
+myNumbers.forEach((number) => {
+    // within the function I can tell what I want to do with the items
+    console.log(number);
+  });
